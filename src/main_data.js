74095f2cf19559187ef26cea57872d05ea71cf0c
farid <farid01729366777@gmail.com>
2025-03-15 00:44:24 +0600

    svg loaded

--- src/main_data.js
+++ src/main_data.js
@@ -1,5 +1,5 @@
 
 // Data loading from data-desing section
 // 1st photo loading 
-const photoLoading=document.getElementById('for_img img')
-
+const photoLoading=document.getElementById('for-img')
+photoLoading.setAttribute('src',course.photo)
