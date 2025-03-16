d0ac2e145ea03eb511ac2ec23a3162279abbe4c2
farid <farid01729366777@gmail.com>
2025-03-16 21:25:49 +0600

    single data design modifyed

--- src/design_data.js
+++ src/design_data.js
@@ -2,10 +2,9 @@
 const course = {
     nameofCourse: 'DeVops Course',
     nameofPerson: 'Mainul islam',
-    lesson: 19,
-    regularPrice: 12000,
-    sellPrice: 5000,
-    studentsAre: 20,
-    photo:'https://interactivecaresbucket.s3.amazonaws.com/main_backend/media/Complete_Video_Editing_Career_Path_Batch_01/Sep-15.2-1.png.webp'
+    lesson: 44,
+    regularPrice: 7000,
+    salePrice: 90,
+    studentsAre: 50,
+    photo: 'https://interactivecaresbucket.s3.amazonaws.com/main_backend/media/Complete_Video_Editing_Career_Path_Batch_01/Sep-15.2-1.png.webp'
 }
- 
--- src/main_data.js
+++ src/main_data.js
@@ -1,5 +1,17 @@
 
 // Data loading from data-desing section
 // 1st photo loading 
-const photoLoading=document.getElementById('for-img')
-photoLoading.setAttribute('src',course.photo)
+const photoLoading = document.getElementById('for-img')
+photoLoading.setAttribute('src', course.photo)
+// lesson data loading 
+const courseLessons = document.getElementById('course-module');
+courseLessons.innerHTML = `Total-lesson-${course.lesson}`
+// students data loading 
+const students = document.getElementById('admission');
+students.innerHTML = `Total ${course.studentsAre}-students`
+
+// price-data-loading (have many twist here )
+const totalPrice = document.querySelector('#all-price span, span');
+totalPrice.innerHTML = course.salePrice > 0 ? ` <span class="font-bold text-yellow-400" >TK:${course.salePrice}</span>
+     <span class="line-through" >TK:${course.regularPrice}</span>` :
+    ` <span class="font-bold text-white shadow animate-pulse" >TK:${course.regularPrice}</span>`
